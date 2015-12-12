9c605481f90ab625b608d62d451b1d30c97d53b7
SimMan <liwei0990@gmail.com>
2015-12-12 15:32:56 +0800

    Initial Commit

---
+++ SMSHelper/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SMSHelper
+//
+//  Created by SimMan on 15/12/12.
+//  Copyright © 2015年 Toutoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
